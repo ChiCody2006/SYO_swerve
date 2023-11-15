d55766f840d7a10d85eba38f99d48ec2590bafaf
ChiCody2006 <131063426+ChiCody2006@users.noreply.github.com>
2023-11-15 00:02:22 -0500

    Comment on Default Elevator Command

--- src/main/java/frc/robot/commands/DefaultElevatorCommand.java
+++ src/main/java/frc/robot/commands/DefaultElevatorCommand.java
@@ -1,5 +1,5 @@
 package frc.robot.commands;
-
+//imports
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -8,12 +8,13 @@ import frc.robot.subsystems.ElevatorSubsystem;
 import frc.robot.subsystems.WinchSubsystem;
 
 public class DefaultElevatorCommand extends CommandBase {
+    //declarations of subsystems and doubles needed for command
     private final ElevatorSubsystem m_elevatorSubsystem;
     private final WinchSubsystem m_winchSubsystem;
 
     private final DoubleSupplier m_elevatorPulleySpeed;
     private final DoubleSupplier m_winchSpeed;
-
+    //command constructor; sets all subsystems and doubles needed to input, added requirements for subsystems
     public DefaultElevatorCommand(ElevatorSubsystem elevatorSubsystem, WinchSubsystem winchSubsystem, DoubleSupplier elevatorPulleySpeed, DoubleSupplier winchSpeed) {
         m_elevatorSubsystem = elevatorSubsystem;
         m_winchSubsystem = winchSubsystem;
@@ -23,12 +24,14 @@ public class DefaultElevatorCommand extends CommandBase {
         addRequirements(elevatorSubsystem, winchSubsystem);
     }
 
+    //When execute is called, set spped of subsystems to corresponding speeds as specified above
     @Override
     public void execute() {
         m_elevatorSubsystem.extend(m_elevatorPulleySpeed.getAsDouble());
         m_winchSubsystem.rotate(m_winchSpeed.getAsDouble());
     }
 
+    //stops the elevator and winch
     @Override
     public void end(boolean interrupted) {
         m_elevatorSubsystem.extend(0);
